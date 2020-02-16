67e9d7b380b32467914bbca1affe9df21af2edad
Xu Lin <shawnlx0701@hotmail.com>
2020-02-15 20:54:38 -0500

    Finish writing the palindrome code

--- palindrome.py
+++ palindrome.py
@@ -2,21 +2,18 @@ from collections import deque
 
 def is_plaindrome(value: str):
     deque_list = deque(value)
-    print(deque_list)
+    new_list = []
+    reverse_list = []
     value_length = int(len(deque_list))
-    print(value_length)
-    count = 0
-    for letter in range(0, value_length//2):
-        while deque_list[letter] == deque_list[value_length-1]:
-            value_length -= 1
-            count += 1
-            print('count:', count)
-            print('letter:', letter)
-            print('length', value_length)
 
-        if count == int(len(deque_list)//2):
-            print(True)
+    for letter in range(0, value_length):
+        new_list.append(deque_list[letter])
+
+    for letter in range(1, value_length+1):
+        reverse_list.append(deque_list[-letter])
+
+    if new_list == reverse_list:
+        print(True)
     else:
         print(False)
-
-is_plaindrome('123321')
+    return reverse_list
