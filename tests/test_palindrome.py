c67681737afe8d2a8b557c64bf9ba33d5dcf42dc
Xu Lin <shawnlx0701@hotmail.com>
2020-02-15 21:29:11 -0500

    test 'a' passed

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -2,5 +2,12 @@ import pytest
 
 from palindrome import is_palindrome
 
+def test_ValueError():
+    with pytest.raises(ValueError):
+        is_palindrome('s')
+
 def test_empty_string():
     assert is_palindrome('') is False
+
+def test_a():
+    assert is_palindrome('a') is True
