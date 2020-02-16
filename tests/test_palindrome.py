08d98538e2cdd745daceab78ee627be6628cbf23
Xu Lin <shawnlx0701@hotmail.com>
2020-02-15 21:31:00 -0500

    test 'bb' passes

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -4,10 +4,13 @@ from palindrome import is_palindrome
 
 def test_ValueError():
     with pytest.raises(ValueError):
-        is_palindrome('s')
+        is_palindrome('(a)')
 
 def test_empty_string():
     assert is_palindrome('') is False
 
 def test_a():
     assert is_palindrome('a') is True
+
+def test_bb():
+    assert is_palindrome('bb') is True
